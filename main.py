ca395f4b4a475d57edcf3ee783caae55296c6224
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-09-09 23:08:20 +0300

    + 1 лаба

--- main.py
+++
@@ -1,7 +0,0 @@
-from first_lab import show_func, tangent_method, chord_method, simple_iterarions_method
-
-if __name__ == '__main__':
-    show_func()
-    tangent_method()
-    chord_method()
-    simple_iterations_method()
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.first_lab import show_func, tangent_method, chord_method, simple_iterations_method
+
+if __name__ == '__main__':
+    show_func()
+    tangent_method()
+    chord_method()
+    simple_iterations_method()
