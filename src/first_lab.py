c5e2c08f3e6914d170a0b90bb0d7eeea8104a72b
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-09-16 01:35:10 +0300

    + fix 1 лабы

--- src/first_lab.py
+++ src/first_lab.py
@@ -32,36 +32,35 @@ def show_func():
 
 def tangent_method():
     # точка, являющаяся границей выбранного нами участка, лежит ближе всего к корню
-    b = 1.5
+    b = 0.2
     # Вписать любое значение большее b, чтобы первое условие цикло верно выполнилось
     iter = 1
 
     while True:
         root = b
         b = b - func(b) / func_derivative(b)
-        print(f'{iter} итерация, корень = {b}')
-        if root - b <= ACCURACY:
+        if b - root < ACCURACY:
             break
+        print(f'{iter} итерация, корень = {b}')
         iter += 1
-    root = b
     print(f'Корень = {root}, корень вычисленный аналитически = {ROOT}')
 
 
 def chord_method():
     # участок, где лежит корень
     a = 0.1
-    b = 2
+    b = 3
     calc_a = (b * func(a) - a * func(b)) / (func(a) - func(b))
     # для выполнения первого условия цикла, боже спаси do-while в питоне((
-    root = a
     iter = 1
 
-    while root > ROOT + ACCURACY or root < ROOT - ACCURACY:
+    while True:
         root = calc_a
         calc_a = (b * func(calc_a) - calc_a * func(b)) / (func(calc_a) - func(b))
+        if root - calc_a <= ACCURACY:
+            break
         print(f'{iter} итерация, корень = {calc_a}')
         iter += 1
-    root = calc_a
     print(f'Корень = {root}, корень вычисленный аналитически = {ROOT}')
 
 
@@ -75,19 +74,6 @@ def first_func(x):
 def second_func(x):
     return x**2
 
-
-def show_third_method():
-    x = np.linspace(0.01, 5, 1000)
-    plt.plot(x, first_func(x), label="$1 / x$")
-    plt.plot(x, second_func(x), label="$x^2$")
-    plt.xlim([0, 5])
-    plt.ylim([-1, 5])
-    plt.grid()
-    plt.legend()
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('$1 / x = x^2$')
-    plt.show()
 
 # Производные функций
 
@@ -107,14 +93,19 @@ def simple_iterations_method():
         calc_a = a
         func_a = second_func(calc_a)
         iter = 1
-        root = 1.5
+        x_coord = [calc_a]
+        y_coord = [func_a]
 
-        while root > ROOT + ACCURACY or root < ROOT - ACCURACY:
+        while True:
             root = calc_a
             calc_a = 1 / func_a
             func_a = second_func(calc_a)
             print(f'{iter} итерация, корень = {calc_a}')
+            if abs(abs(root) - abs(calc_a)) <= ACCURACY:
+                break
             iter += 1
+            x_coord.append(calc_a)
+            y_coord.append(func_a)
         root = calc_a
         print(f'Корень = {root}, корень вычисленный аналитически = {ROOT}')
     else:
@@ -122,12 +113,31 @@ def simple_iterations_method():
         func_a = first_func(calc_a)
         iter = 1
         root = 1.5
+        x_coord = [calc_a]
+        y_coord = [func_a]
 
-        while root > ROOT + ACCURACY or root < ROOT - ACCURACY:
+        while True:
             root = calc_a
             calc_a = math.sqrt(func_a)
             func_a = first_func(calc_a)
             print(f'{iter} итерация, корень = {calc_a}')
+            if abs(abs(root) - abs(calc_a)) <= ACCURACY:
+                break
             iter += 1
+            x_coord.append(calc_a)
+            y_coord.append(func_a)
         root = calc_a
         print(f'Корень = {root}, корень вычисленный аналитически = {ROOT}')
+
+        x = np.linspace(0.01, 5, 1000)
+        plt.plot(x, first_func(x), label="$1 / x$")
+        plt.plot(x, second_func(x), label="$x^2$")
+        plt.plot(x_coord, y_coord, label="$решение$")
+        plt.xlim([0.5, 1.5])
+        plt.ylim([0.6, 2])
+        plt.grid()
+        plt.legend()
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('$1 / x = x^2$')
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-from src.first_lab import show_func, tangent_method, chord_method, simple_iterations_method, show_third_method
+from src.first_lab import show_func, tangent_method, chord_method, simple_iterations_method
 
 if __name__ == '__main__':
     show_func()
     tangent_method()
     chord_method()
     simple_iterations_method()
-    show_third_method()
