736f76564bc60737fbd1f6b4e31fa27f94f20467
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-10-02 18:22:11 +0300

    + 2 лаба

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from src.first_lab import show_func, tangent_method, chord_method, simple_iterations_method
+from src.second_lab import show_func, square_method, trapezoid_method, simpson_method
 
 if __name__ == '__main__':
-    show_func()
+    """ show_func()
     tangent_method()
     chord_method()
-    simple_iterations_method()
+    simple_iterations_method() """
+    show_func()
+    square_method()
+    trapezoid_method()
+    simpson_method()
---
+++ src/second_lab.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+A, B = 1, 3
+SUM = 5.72291136766
+
+
+def func(x):
+    return x ** 2 - x * np.log(x)
+
+
+def show_func():
+    x = np.linspace(0.01, 5)
+    fig, ax = plt.subplots()
+    ax.plot(x, func(x))
+    ax.set_xlim([0, 5])
+    ax.set_ylim([0, 7])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('$y = x^2 - x * ln(x)$')
+
+    ix = np.linspace(A, B)
+    iy = func(ix)
+    verticals = [(A, 0), *zip(ix, iy), (B, 0)]
+    poly = Polygon(verticals, facecolor='0.9', edgecolor='0.5')
+    ax.add_patch(poly)
+
+    ax.text(0.5 * (A + B), 0.8, r"$\int_a^b f(x)\mathrm{d}x$",
+            horizontalalignment='center', fontsize=15)
+
+    plt.show()
+
+
+def square_method():
+    # На сколько кусочков разобьем интервал
+    n = 10000
+    h = (B - A) / n
+    x = np.linspace(A, B, n)
+    sum = 0
+    for i in range(n - 1):
+        sum += func((x[i] + x[i + 1]) / 2) * h
+
+    print(f'Значение интеграла = {sum}, вычисленное с помощью специализированного ПО = {SUM}')
+
+
+def trapezoid_method():
+    n = 10000
+    h = (B - A) / n
+    x = np.linspace(A, B, n)
+    sum = 0
+    for i in range(n - 1):
+        sum += ((func(x[i]) + func(x[i + 1])) / 2) * h
+
+    print(f'Значение интеграла = {sum}, вычисленное с помощью специализированного ПО = {SUM}')
+
+
+def simpson_method():
+    n = 10000
+    h = (B - A) / n
+    x = np.linspace(A, B, n)
+    sum = 0
+    for i in range(1, n):
+        sum += (func(x[i - 1]) + 4 * func((x[i - 1] + x[i]) / 2) + func(x[i])) / 6 * h
+
+    print(f'Значение интеграла = {sum}, вычисленное с помощью специализированного ПО = {SUM}')
